f89dd3b5ccada4fefb3f668c943bce2f0804f4e7
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-10-19 17:43:36 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = 'http://www.omdbapi.com?apiKey=8598c3f';
+
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "2010",
+    "imdbID": "tt1785572",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_KEY}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies("Spiderman");
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Movie Land</h1>
+
+            <div className='search'>
+                <input
+                    placeholder="search for movies"
+                    value="Superman"
+                    onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+               <MovieCard movie1={movie1}/>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
