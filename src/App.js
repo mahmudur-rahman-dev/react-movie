3316760abfe24b6e441f9fbdefd3516e79a45770
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-10-19 23:25:49 +0600

    add component to show dynamic list of movies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import { useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import './App.css'
 import SearchIcon from './search.svg'
 import MovieCard from './MovieCard'
 
 const API_KEY = 'http://www.omdbapi.com?apiKey=8598c3f';
 
-const movie1 = {
-    "Title": "Spiderman",
-    "Year": "2010",
-    "imdbID": "tt1785572",
-    "Type": "movie",
-    "Poster": "N/A"
-}
 const App = () => {
+    const [movies, setMovies] = useState([]);
     const searchMovies = async (title) => {
         const response = await fetch(`${API_KEY}&s=${title}`);
         const data = await response.json();
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -31,18 +25,30 @@ const App = () => {
                 <input
                     placeholder="search for movies"
                     value="Superman"
-                    onChange={() => {}}
+                    onChange={() => {
+                    }}
                 />
                 <img
                     src={SearchIcon}
                     alt="search"
-                    onClick={() => {}}
+                    onClick={() => {
+                    }}
                 />
             </div>
 
-            <div className="container">
-               <MovieCard movie1={movie1}/>
-            </div>
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map(movie => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )
+            }
 
         </div>
     );
